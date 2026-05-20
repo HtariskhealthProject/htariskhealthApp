7945636950a6e8b551755a6406c0758c95042039
Vanet0 <vanessat0422@gmail.com>
2026-05-19 20:23:05 -0500

    Cambiar color del panel izquierdo a azul

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -46,7 +46,7 @@ export default function Sidebar({ open, onClose }: Props) {
       )}
 
       <aside
-        className={`fixed top-0 left-0 z-50 h-full w-[260px] bg-white border-r border-slate-200 transform transition-transform duration-200 ease-in-out lg:translate-x-0 lg:static lg:z-auto flex flex-col ${
+        className={`fixed top-0 left-0 z-50 h-full w-[260px] bg-[#0E273F] border-r border-[#0E273F] transform transition-transform duration-200 ease-in-out lg:translate-x-0 lg:static lg:z-auto flex flex-col ${
           open ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
